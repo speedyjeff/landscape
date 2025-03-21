1a06d67b2f18b98b265f00c5bf09c70bba2221a5
speedyjeff <speedyjeff@github.com>
2025-03-21 14:28:13 -0700

    initial start to a 3d landscape environment

---
+++ Airplane.cs
@@ -0,0 +1,22 @@
+﻿using engine.Common.Entities3D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design.Behavior;
+
+namespace landscape
+{
+    class Airplane : ComboElement3D
+    {
+        public Airplane()
+        {
+            // airplane fuselage
+            var body = new Cylinder() { X = X, Y = Y, Z = Z, };
+
+            // todo
+
+        }
+    }
+}
